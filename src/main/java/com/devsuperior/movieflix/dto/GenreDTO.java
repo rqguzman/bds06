3927c8b6c6f39411945cd90f889a2c90f9341c1e
rqguzman <rafael.guzman@outlook.com>
2021-10-13 14:35:56 -0300

    :recycle: Remove unnecessary collection from GenreDTO

--- src/main/java/com/devsuperior/movieflix/dto/GenreDTO.java
+++ src/main/java/com/devsuperior/movieflix/dto/GenreDTO.java
@@ -1,18 +1,14 @@
 package com.devsuperior.movieflix.dto;
 
 import com.devsuperior.movieflix.entities.Genre;
-import com.devsuperior.movieflix.entities.Movie;
 
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
 
 public class GenreDTO implements Serializable {
 
     private static final long serialVersionUID = 3514923998907115091L;
     private Long id;
     private String name;
-    private Set<MovieDTO> movies = new HashSet<>();
 
     public GenreDTO() {
     }
@@ -25,11 +21,6 @@ public class GenreDTO implements Serializable {
     public GenreDTO(Genre entity) {
         id = entity.getId();
         name = entity.getName();
-    }
-
-    public GenreDTO(Genre entity, Set<Movie> movies) {
-        this(entity);
-        movies.forEach(movie -> this.movies.add(new MovieDTO(movie)));
     }
 
     public Long getId() {
@@ -48,7 +39,4 @@ public class GenreDTO implements Serializable {
         this.name = name;
     }
 
-    public Set<MovieDTO> getMovies() {
-        return movies;
-    }
 }
